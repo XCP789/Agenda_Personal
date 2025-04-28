0f7d8b99ea46ba4f05375680aba74c84ba615096
Arturo ED <xcorpio789@gmail.com>
2025-04-27 23:03:51 -0700

    Se intento hacer que en configuracion se pueda cambiar el tema al presionar el boton "Cambiar Tema", des[ues de varios intentos no hubo exito.

--- Agenda_Personal/ConfiguracionPage.xaml.cs
+++ Agenda_Personal/ConfiguracionPage.xaml.cs
@@ -6,4 +6,27 @@ public partial class ConfiguracionPage : ContentPage
 	{
 		InitializeComponent();
 	}
+	private async void CambiarTema(object sender, EventArgs e)
+	{
+		bool TemaOscuro = Application.Current.Resources["FondoColor"].ToString() == "#1E1E1E";
+
+		Application.Current.Resources["FondoColor"] = TemaOscuro ? Color.FromArgb("#e8e8e8") : Color.FromArgb("#1E1E1E");
+        Application.Current.Resources["TextColor"] = TemaOscuro ? Color.FromArgb("#000000") : Color.FromArgb("#ffffff");
+
+		//var paginaActual = Application.Current.MainPage;
+		//Application.Current.MainPage = new ContentPage();
+
+		//Application.Current.MainPage = paginaActual;
+
+		Dispatcher.Dispatch(() =>
+		{
+			Application.Current.MainPage.BackgroundColor = (Color)Application.Current.Resources["FondoColor"];
+		});
+
+		await Task.Delay(50);
+        //foreach (var pagina in Application.Current.MainPage.Navigation.NavigationStack)
+        //{
+        //    pagina.BackgroundColor = (Color)Application.Current.Resources["FondoColor"];
+        //}
+    }
 }
